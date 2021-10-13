a51abfbda5c218d017b83fdb4050ca14e53ee186
Kakip1919 <invest9me@yahoo.co.jp>
2021-10-13 23:16:57 +0900

    1st

---
+++ controller.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="jp">
+<head>
+    <meta charset="utf-8">
+    <title>文字数カウンタ</title>
+</head>
+<body>
+<form action=word.php method="post">
+    <input name="use" type=“text”>
+    <button type="submit">送信</button>
+</form>
+
+</body>
+</html>
---
+++ word.php
@@ -0,0 +1,9 @@
+<?php
+
+//
+$text = $_POST["use"];
+
+
+$text_count = mb_strlen($text,'UTF-8');
+var_dump($text_count);
+echo $text_count;
